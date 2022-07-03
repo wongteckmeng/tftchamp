772700f644b0f039ac342237c61ea737d6db7d75
furyhawk <furyx@hotmail.com>
2022-07-04 00:57:51 +0800

    traits_col unique items augments config fixes

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,26 +4,26 @@
 from wrappers import *
 
 from sklearn.svm import SVC
-from sklearn.ensemble import ExtraTreesClassifier, VotingClassifier
+from sklearn.ensemble import ExtraTreeRegressor, VotingRegressor
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import PolynomialFeatures
 
 from sklearn.linear_model import Ridge
 from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPRegressor
 
 from xgboost import XGBRegressor
 
 # Soft Voting/Majority Rule classifier for unfitted estimators.
-clf1 = ExtraTreesClassifier(n_estimators=300, class_weight="balanced")
+clf1 = ExtraTreeRegressor(n_estimators=300)
 clf2 = XGBRegressor(objective="reg:squarederror",
-                     eval_metric="mae", n_estimators=300, max_depth=12)
-clf3 = MLPClass()
+                     eval_metric="mae", n_estimators=300, max_depth=9)
+clf3 = MLPKerasRegressor()
 # clf3 = MLPClassifier(hidden_layer_sizes=(100, 50, 25, 10), max_iter=1000, early_stopping=True)
 
 
-eclf = VotingClassifier(estimators=[
+eclf = VotingRegressor(estimators=[
     ('et', clf1), ('xgb', clf2), ('mlp', clf3)], voting='soft')
 
 methods_dict = {
@@ -33,12 +33,12 @@ methods_dict = {
     'preprocessing': NoShowPreprocessing,
     'column_transformer': Column_Wrapper,
     'PLS': PLSRegressionWrapper,
-    'MLPClass': MLPClass,
+    'MLPClass': MLPKerasRegressor,
     'GNB': GaussianNB,
     'SVC': SVC,
     'PCA': PCA,
-    'ETC': ExtraTreesClassifier,
-    'MLP': MLPClassifier,
+    'ETC': ExtraTreeRegressor,
+    'MLP': MLPRegressor,
     'XGB': XGBRegressor,
     'VCL': eclf,
 }
