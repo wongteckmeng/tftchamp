0d49abd17539d4d24756169d490c1c47594f698e
Teck Meng <furyx@hotmail.com>
2022-07-15 15:00:55 +0800

    hotfix early close

--- backend/app/scrape.py
+++ backend/app/scrape.py
@@ -17,9 +17,9 @@ LOAD_NEW: bool = False
 ASSETS_DIR: str = settings.assets_dir
 API_KEY: str = settings.api_key
 SERVER = 'na1'  # ['euw1', 'na1', 'kr', 'oc1']
-LEAGUE = 'grandmasters'  # ['challengers', 'grandmasters']
+LEAGUE = 'challengers'  # ['challengers', 'grandmasters']
 
-MAX_COUNT: int = 30
+MAX_COUNT: int = 50
 
 
 def requestsLog(url, status, headers):
