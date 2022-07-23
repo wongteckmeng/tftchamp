933ce0122c788fe168c57f7ca11afee89934e138
Teck Meng <furyx@hotmail.com>
2022-07-23 20:22:40 +0800

    ADD window batch run scripts

---
+++ backend/app/run_preprocess.bat
@@ -0,0 +1,4 @@
+python3 data_loading.py -c configs/challengers_loading.json
+python3 data_loading.py -c configs/grandmasters.json
+python3 team_composition.py -c configs/challengers_loading.json
+python3 team_composition.py -c configs/grandmasters.json
