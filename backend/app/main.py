163f0ec38c46ab424823af165d8553c3ddbe23a6
furyhawk <furyx@hotmail.com>
2022-07-31 14:40:11 +0800

    client definition

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
-import motor.motor_asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from routers.matchdetails import router as matchdetails_router
 from routers.matches import router as matches_router
 from config import settings
 
 app = FastAPI()
-app.include_router(matchdetails_router, tags=[
-                   "matchdetails"], prefix="/matchdetail")
-app.include_router(matches_router, tags=[
-                   "matches"], prefix="/match")
 
 
 @app.on_event("startup")
 async def startup_db_client():
-    app.mongodb_client = motor.motor_asyncio.AsyncIOMotorClient(settings.db_uri)
+    app.mongodb_client = AsyncIOMotorClient(settings.db_uri)
     app.database = app.mongodb_client[settings.db_name]
 
 
@@ -23,6 +19,10 @@ async def startup_db_client():
 async def shutdown_db_client():
     app.mongodb_client.close()
 
+app.include_router(matchdetails_router, tags=[
+                   "matchdetails"], prefix="/matchdetail")
+app.include_router(matches_router, tags=[
+                   "matches"], prefix="/match")
 
 if __name__ == "__main__":
     uvicorn.run(
