72d7c8854df5c96ecf5c7c78e9b343c8a3e7e142
furyhawk <furyx@hotmail.com>
2022-08-03 23:15:28 +0800

    todo react zustand test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard'
 import Person from './components/Person';
+import Todo from './components/Todo'
 import Vote from './components/Vote';
 
 function App() {
     return (
         <>
             {/* <Dashboard /> */}
+            <Todo />
             <Person />
             <Vote />
         </>
