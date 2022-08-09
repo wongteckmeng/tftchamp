71cc992368e1e9268c326fd82e434e62dabeb15b
furyhawk <furyx@hotmail.com>
2022-08-10 00:05:03 +0800

    TEST: QueryClientProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,30 +5,34 @@ import Toolbar from '@mui/material/Toolbar';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
-
+// import { QueryClientProvider, QueryClient } from 'react-query';
 // import Dashboard from './components/Dashboard'
 // import Person from './components/Person';
 // import Todo from './components/Todo'
 // import Vote from './components/Vote';
 import Match from './components/Match';
 
+// const queryClient = new QueryClient();
+
 function App() {
     return (
         <>
-            <AppBar position="static">
-                <Toolbar variant="dense">
-                    <VideogameAssetIcon sx={{ mr: 2 }} />
-                    <Typography variant="h5" color="inherit" component="div">
-                        TFTChamp
-                    </Typography>
-                    <Box sx={{ flexGrow: 1 }} />
-                </Toolbar>
-            </AppBar>
-            {/* <Dashboard /> */}
-            <Match />
-            {/* <Todo />
+            {/* <QueryClientProvider client={queryClient}> */}
+                <AppBar position="static">
+                    <Toolbar variant="dense">
+                        <VideogameAssetIcon sx={{ mr: 2 }} />
+                        <Typography variant="h5" color="inherit" component="div">
+                            TFTChamp
+                        </Typography>
+                        <Box sx={{ flexGrow: 1 }} />
+                    </Toolbar>
+                </AppBar>
+                {/* <Dashboard /> */}
+                <Match />
+                {/* <Todo />
             <Person />
             <Vote /> */}
+            {/* </QueryClientProvider> */}
         </>
     );
 }
