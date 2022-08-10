5121c4593c7b44e152c88a31bcee2f1d80398997
furyhawk <furyx@hotmail.com>
2022-08-10 23:52:30 +0800

    UPDATE: store unique matches

--- frontend/src/store/MatchStore.js
+++ frontend/src/store/MatchStore.js
@@ -10,13 +10,18 @@ import create from 'zustand'
 
 const uri = "http://localhost:8000/match/?platform=na1&skip=0&limit=5";
 
-const useStore = create((set) => ({
+const useStore = create((set, get) => ({
     uri: uri,
+    count: 0,
     Matches: [],
     fetch: async (uri) => { //: RequestInfo | URL
         const response = await fetch(uri);
         const json = await response.json();
-        set({ Matches: json })
+        let result = json.result;
+        let new_array = [...get().Matches, ...result];
+        let res = new_array.splice(1).reduce((acc, elem) => acc.every(({_id}) => _id !== elem._id) ? [...acc, elem] : acc, [new_array[0]]);
+        set({ count: json.count });
+        set({ Matches: res});
     },
 }));
 
