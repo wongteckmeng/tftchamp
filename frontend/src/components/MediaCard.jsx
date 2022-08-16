c36f51f321bf6d9dc7a1fb003aab64b49090cea0
furyhawk <furyx@hotmail.com>
2022-08-17 00:10:17 +0800

    ADD: MediaCard

--- frontend/src/components/MediaCard.jsx
+++ frontend/src/components/MediaCard.jsx
@@ -1,45 +1,73 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Skeleton from '@mui/material/Skeleton';
 
 export default function MediaCard() {
-    const uri = "http://localhost:8000/image/na1_challengers_12.15.458.1416_2022-08-10_feature_importances?platform=na1&league=challengers&version=12.15.458.1416&patch=2022-08-10";
-    // const [image, setImage] = React.useState([])  
+    const [images, setImages] = React.useState([]);
 
-    // useEffect(() => {
-    //     fetch: async (uri) => { //: RequestInfo | URL
-    //     const response = await fetch(uri);
-    //     setImage(response)
+    const imageBase = `http://localhost:8000/image/`;
+    const imageQuery = `?platform=na1&league=challengers&version=12.15.458.1416&patch=2022-08-10`;
+    const uri = `http://localhost:8000/image/${imageQuery}`
 
-    // }, [uri]); 
+    React.useEffect(() => {
+        let canceled = false;
+        async function getImages(uri) {
+            if (!canceled) {
+                const response = await fetch(uri);
+                const data = await (response.json());
+                setImages(data.results);
+            }
+        }
+
+        getImages(uri);
+        return () => canceled = true;
+    }, [uri])
+
 
     return (
-        <Card sx={{ maxWidth: 1000 }}>
-            <CardMedia
-                component="img"
-                image={uri}
-                alt="Feature Importance"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                Feature Importance
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                Feature Importance refers to techniques that calculate a score
-                for all the input features for a given model —
-                the scores simply represent the “importance” of each feature.
-                A higher score means that the specific feature will have a larger
-                effect on the model that is being used to predict a certain variable.
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-            </CardActions>
-        </Card>
+        <Box
+            sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                '& > :not(style)': {
+                    m: 1,
+                },
+            }}
+        >
+            <Paper sx={{ width: '100%', minHeight: '500', overflow: 'hidden' }}>
+                {images.length ? (
+                    images.map((image) => (
+                        <Card key={image.uri} sx={{ maxWidth: 1200, minHeight: '500' }}>
+                            <CardContent>
+                                <Typography gutterBottom variant="h4" component="div">
+                                    {image.uri}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {image.description}
+                                </Typography>
+                            </CardContent>
+                            <CardMedia
+                                component="img"
+                                image={imageBase + image.uri + imageQuery}
+                                alt={image.uri}
+                            />
+
+                            <CardActions>
+                                <Button size="small">Share</Button>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    ))
+                ) : (<Skeleton variant="rectangular" width="100%" height={300} />)}
+            </Paper >
+
+        </Box >
     );
 }
