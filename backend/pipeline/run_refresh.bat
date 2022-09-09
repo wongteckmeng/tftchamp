77cc9b9e3f3f30387d28431bb615be42b0e3e753
Teck Meng <furyx@hotmail.com>
2022-09-09 12:49:42 +0800

    Update run_refresh.bat

--- backend/pipeline/run_refresh.bat
+++ backend/pipeline/run_refresh.bat
@@ -1,4 +1,4 @@
-python scrape_db.py -c configs/challengers.json --no-load_new
+@REM python scrape_db.py -c configs/challengers.json --no-load_new
 @REM python scrape_db.py -c configs/challenger_oce.json --no-load_new
-python scrape_db.py -c configs/grandmasters.json --no-load_new
+@REM python scrape_db.py -c configs/grandmasters.json --no-load_new
 python scrape_db.py -c configs/masters.json --no-load_new
