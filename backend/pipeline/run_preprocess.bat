478fd233c3eff031838147afedc53fa466f5ac15
furyhawk <furyx@hotmail.com>
2022-09-11 19:34:58 +0800

    220911 master run

--- backend/pipeline/run_preprocess.bat
+++ backend/pipeline/run_preprocess.bat
@@ -1,12 +1,12 @@
 python data_loading_db.py -c configs/challengers.json
 python data_loading_db.py -c configs/grandmasters.json
-python data_loading_db.py -c configs/masters.json
+@REM python data_loading_db.py -c configs/masters.json
 python team_composition_db.py -c configs/challengers.json
 python team_composition_db.py -c configs/grandmasters.json
-python team_composition_db.py -c configs/masters.json
-python optimizer.py -c configs/config_xgb_m.json
-python optimizer.py -c configs/config_xgb_m_euw.json
-python optimizer.py -c configs/config_xgb_m_kr.json
+@REM python team_composition_db.py -c configs/masters.json
+@REM python optimizer.py -c configs/config_xgb_m.json
+@REM python optimizer.py -c configs/config_xgb_m_euw.json
+@REM python optimizer.py -c configs/config_xgb_m_kr.json
 python optimizer.py -c configs/config_xgb_gm.json
 python optimizer.py -c configs/config_xgb_euw.json
 python optimizer.py -c configs/config_xgb_kr.json
