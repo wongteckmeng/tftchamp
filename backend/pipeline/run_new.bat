b49d51f24c91d647ca65fd4e1cf5dfd84d5f2ce2
Teck Meng <furyx@hotmail.com>
2022-09-11 19:33:55 +0800

    master

--- backend/pipeline/run_new.bat
+++ backend/pipeline/run_new.bat
@@ -1,3 +1,3 @@
-@REM python scrape_db.py -c configs/challengers.json --load_new -m 1
-@REM python scrape_db.py -c configs/grandmasters.json --load_new -m 1
-python scrape_db.py -c configs/masters.json --load_new -m 1
+python scrape_db.py -c configs/challengers.json --load_new -m 1
+python scrape_db.py -c configs/grandmasters.json --load_new -m 1
+@REM python scrape_db.py -c configs/masters.json --load_new -m 1
